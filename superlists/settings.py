bb6739ee7083ae25b5148a7be40241a4c7baf3ae
root <root@localhost.localdomain>
2020-05-30 21:23:12 +0800

    DD Gunicorn to virtualenv requirements

--- superlists/settings.py
+++ superlists/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '61u9!-snw-@)6^$i--8b%uuuj#1(agdn)c#f5!c43=@m1-wl)t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = ['49.235.239.120']
 
 
 # Application definition
